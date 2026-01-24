0bc8ac69ba93d1ced5e6bc94c51f221bfa63dc5f
dofixpooja <pujakumarirai109@gmail.com>
2026-01-24 15:08:52 +0530

    added roles in table for admin api

--- routes/api.php
+++ routes/api.php
@@ -22,14 +22,14 @@ Route::delete('/categories/{id}', [CategoryController::class, 'destroy']); // DE
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
-// Route::middleware('auth:sanctum')->group(function () {
+Route::middleware('auth:sanctum')->group(function () {
 
-//     Route::post('/logout', [AuthController::class, 'logout']);
+    Route::post('/logout', [AuthController::class, 'logout']);
 
-//     Route::get('/categories', [CategoryController::class, 'index']);
-//     Route::get('/categories/{id}', [CategoryController::class, 'show']);
-//     Route::post('/categories', [CategoryController::class, 'store']);
-//     Route::put('/categories/{id}', [CategoryController::class, 'update']);
-//     Route::delete('/categories/{id}', [CategoryController::class, 'destroy']);
+    // Route::get('/categories', [CategoryController::class, 'index']);
+    // Route::get('/categories/{id}', [CategoryController::class, 'show']);
+    // Route::post('/categories', [CategoryController::class, 'store']);
+    // Route::put('/categories/{id}', [CategoryController::class, 'update']);
+    // Route::delete('/categories/{id}', [CategoryController::class, 'destroy']);
 
-// });
+});
